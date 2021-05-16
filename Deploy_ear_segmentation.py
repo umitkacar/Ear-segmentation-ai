1f1982fee0493b76a56daf527695d7b48e11ba14
Senior R&D Engineer <umitkacar@itu.edu.tr>
2021-05-16 21:23:19 +0300

    Add files via upload

--- Deploy_ear_segmentation.py
+++ Deploy_ear_segmentation.py
@@ -9,7 +9,7 @@ os.environ["CUDA_VISIBLE_DEVICES"]=""
 
 # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-LOAD_MODEL_DEPLOY_PATH = "./model_ear/model_ear_segmentation.pth"
+LOAD_MODEL_DEPLOY_PATH = "./model_ear/best_model_ear_v1_43.pth"
 ENCODER = 'resnet18'
 ENCODER_WEIGHTS = 'imagenet'
 CLASSES = ['optic_disc']
@@ -19,7 +19,7 @@ DEVICE = "cpu"
 def get_validation_augmentation():
     """Add paddings to make image shape divisible by 32"""
     test_transform = [
-          Resize(height=480, width=640, always_apply=True),
+          Resize(height=320, width=480, always_apply=True),
     ]
     return Compose(test_transform)
 
@@ -57,7 +57,7 @@ if __name__ == "__main__":
     preprocessing_fn = smp.encoders.get_preprocessing_fn(ENCODER, ENCODER_WEIGHTS)
     preprocessing = get_preprocessing(preprocessing_fn)
 
-    data_dir =  ["./test-ear-image"]
+    data_dir =  ["./test-images"]
 
     data_samples = []
     for _dir in data_dir:
@@ -68,7 +68,7 @@ if __name__ == "__main__":
     for path in data_samples:
 
         img = cv2.imread(path)
-        img = cv2.resize(img, (640,480))
+        img = cv2.resize(img, (480,320))
         h, w = img.shape[:2]
         image = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
